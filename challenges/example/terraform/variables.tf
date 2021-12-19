01fee37ad9488785feda188a982ba2c3e35acff6
Coin Graham <coin.graham@cleardata.com>
2021-12-19 12:55:07 -0500

    generalize example

--- challenges/example/terraform/variables.tf
+++ challenges/example/terraform/variables.tf
@@ -1,14 +1,1 @@
 # This file is where you will define your variables
-
-# VPC Variables
-variable "vpc_name" {
-  type = "string"
-}
-
-variable "cidr_block" {
-  type = "string"
-}
-
-variable "tags" {
-  type = "map"
-}
